e3edcabf0456dc9e840106416cb0dd1c5a1f9da1
valentin.genchev <valentin.genchev@CMW8LH2>
2022-06-29 11:02:14 +0300

    Add project files.

---
+++ HR.LeaveManagement.Identity/Models/ApplicationUser.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace HR.LeaveManagement.Identity.Models
+{
+    public class ApplicationUser : IdentityUser
+    {
+        public string FirstName { get; set; }
+        public string MiddleName { get; set; }
+        public string LastName { get; set; }
+        public string BirthPlace { get; set; }
+        public DateOnly BirthDate { get; set; }
+        public string Country { get; set; }
+        public string Address { get; set; }
+        public string IdCard { get; set; }
+        public DateOnly IdCardValidity { get; set; }
+        public string CardAuthority { get; set; }
+        public string BSN { get; set; }
+        public string IBAN { get; set; }
+        public string BIC { get; set; }
+        public string BankDetails { get; set; }
+        public int WorkExperienceInDays { get; set; }
+        public DateOnly StartDate { get; set; }
+        public int? WorkExperienceInCompanyInDays { get; private set; }
+        public int VacationHours { get; set; }
+        public string Office { get; set; }
+        public string Discipline { get; set; }
+        public string Function { get; set; }
+        public string Team { get; set; }
+    }
+}
