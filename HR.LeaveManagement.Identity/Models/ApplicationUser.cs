6d621a1ab88d6cad7a3791933798a62e46abe3a3
Valentin <valentin.genchev3@gmail.com>
2022-06-29 11:18:05 +0300

    a

--- HR.LeaveManagement.Identity/Models/ApplicationUser.cs
+++ HR.LeaveManagement.Identity/Models/ApplicationUser.cs
@@ -8,13 +8,13 @@ namespace HR.LeaveManagement.Identity.Models
     public class ApplicationUser : IdentityUser
     {
         public string FirstName { get; set; }
-        public string MiddleName { get; set; }
+        public string? MiddleName { get; set; }
         public string LastName { get; set; }
-        public string BirthPlace { get; set; }
-        public DateOnly BirthDate { get; set; }
-        public string Country { get; set; }
-        public string Address { get; set; }
-        public string IdCard { get; set; }
+        public string? BirthPlace { get; set; }
+        public DateOnly? BirthDate { get; set; }
+        public string? Country { get; set; }
+        public string? Address { get; set; }
+        public string? IdCard { get; set; }
         public DateOnly IdCardValidity { get; set; }
         public string CardAuthority { get; set; }
         public string BSN { get; set; }
